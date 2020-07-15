826c39bc12d250783c50c677de513c1eaf204bfc
Dmitry Gorochovsky <dgoro@yahoo.com>
2020-07-15 15:17:42 +0300

    add example

---
+++ Example/Lambda/go.mod
@@ -0,0 +1,10 @@
+module github.com/akeylesslabs/akeyless-go-cloud-id
+
+go 1.13
+
+require (
+	github.com/akeylesslabs/akeyless-go-sdk v0.1.4
+	github.com/antihax/optional v1.0.0
+	github.com/aws/aws-lambda-go v1.17.0
+	github.com/aws/aws-sdk-go-v2 v0.23.0
+)
---
+++ Example/Lambda/lambda_function.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/antihax/optional"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	akl_cloud_id "github.com/akeylesslabs/akeyless-go-cloud-id"
+	akl "github.com/akeylesslabs/akeyless-go-sdk"
+)
+
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func HandleRequest() (string, error) {
+
+	cloud_id, err := akl_cloud_id.GetCloudId()
+	if err != nil {
+		fmt.Println("GetCloudId error:", err.Error())
+		panic(err.Error())
+	}
+
+	cfg := akl.NewConfiguration()
+	cfg.BasePath = "http://<api-gateway-host>:<port>"
+	client := akl.NewAPIClient(cfg)
+	api := client.DefaultApi
+	aklsCtx := context.Background()
+
+	accessId := "<auth-method-access-id>"
+	secretName := "<secret-name>"
+
+	// Authenticate to the service and returns an access token
+	authReplyObj, _, err := api.Auth(aklsCtx, accessId, &akl.AuthOpts{
+		AccessType: optional.NewString("aws_iam"),
+		CloudId:    optional.NewString(cloud_id),
+	})
+	if err != nil {
+		fmt.Println("Auth error:", err.Error())
+		panic("Auth failed")
+	}
+
+	token := authReplyObj.Token
+
+	getValReplyObj, _, err := api.GetSecretValue(aklsCtx, secretName, token)
+	if err != nil {
+		fmt.Println("GetSecretValue error:", err.Error())
+		panic("GetSecretValue failed")
+	}
+	fmt.Println("Secret value:", getValReplyObj.Response[0])
+
+	return "", nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
