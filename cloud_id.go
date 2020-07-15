d2a961a08d50528891388dfcfa2b406df5f7ec32
Dmitry Gorochovsky <dgoro@yahoo.com>
2020-07-15 09:49:59 +0300

    initial commit

---
+++ cloud_id.go
@@ -0,0 +1,52 @@
+package akeyless_cloud_id
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+func GetCloudId() (string, error) {
+	awsCfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		return "", err
+	}
+
+	// caller identity request can be only us-east-1. Default call brings region where caller is
+	awsCfg.Region = "us-east-1"
+
+	svc := sts.New(awsCfg)
+	input := &sts.GetCallerIdentityInput{}
+	req := svc.GetCallerIdentityRequest(input)
+
+	err = req.Sign()
+	if err != nil {
+		return "", err
+	}
+
+	headersJson, err := json.Marshal(req.HTTPRequest.Header)
+	if err != nil {
+		return "", err
+	}
+	requestBody, err := ioutil.ReadAll(req.HTTPRequest.Body)
+	if err != nil {
+		return "", err
+	}
+
+	awsData := make(map[string]string)
+	awsData["sts_request_method"] = req.HTTPRequest.Method
+	awsData["sts_request_url"] = base64.StdEncoding.EncodeToString([]byte(req.HTTPRequest.URL.String()))
+	awsData["sts_request_body"] = base64.StdEncoding.EncodeToString(requestBody)
+	awsData["sts_request_headers"] = base64.StdEncoding.EncodeToString(headersJson)
+	awsDataDump, err := json.Marshal(awsData)
+
+	if err != nil {
+		return "", err
+	}
+
+	cloud_id := base64.StdEncoding.EncodeToString(awsDataDump)
+	return cloud_id, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/akeylesslabs/cloud-id
+
+go 1.13
+
+require github.com/aws/aws-sdk-go-v2 v0.23.0
