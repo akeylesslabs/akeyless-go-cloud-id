f282c6d0f5557817b9adadc4970b98f3b00acb27
Alexey <alikdolg@gmail.com>
2026-05-11 11:17:58 +0300

    Go 1.62, AWS SDK V2, support AWS China, run tests

--- cloudprovider/aws/cloud_id.go
+++ cloudprovider/aws/cloud_id.go
@@ -1,45 +1,76 @@
 package aws
 
 import (
+	"bytes"
+	"context"
+	"crypto/sha256"
 	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
-	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sts"
+	awssdk "github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+const (
+	defaultSTSRegion = "us-east-1"
+	stsServiceName   = "sts"
 )
 
 func GetCloudId() (string, error) {
-	// Endpoint https://sts.amazonaws.com is available only in single region: us-east-1.
-	// So, caller identity request can be only us-east-1. Default call brings region where caller is
-	region := "us-east-1"
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		return "", err
+	}
+	return getCloudID(context.Background(), cfg, time.Now())
+}
 
-	sess, err := session.NewSession()
+func getCloudID(ctx context.Context, cfg awssdk.Config, signingTime time.Time) (string, error) {
+	region := cfg.Region
+	if region == "" {
+		region = defaultSTSRegion
+	}
+
+	endpointURL, err := resolveSTSEndpoint(ctx, cfg, region)
 	if err != nil {
 		return "", err
 	}
 
-	svc := sts.New(sess, aws.NewConfig().WithRegion(region))
-	input := &sts.GetCallerIdentityInput{}
-	req, _ := svc.GetCallerIdentityRequest(input)
+	requestBody := []byte(url.Values{
+		"Action":  {"GetCallerIdentity"},
+		"Version": {"2011-06-15"},
+	}.Encode())
 
-	if err := req.Sign(); err != nil {
-		return "", err
-	}
-
-	headersJson, err := json.Marshal(req.HTTPRequest.Header)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpointURL.String(), bytes.NewReader(requestBody))
 	if err != nil {
 		return "", err
 	}
-	requestBody, err := ioutil.ReadAll(req.HTTPRequest.Body)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	credentials, err := cfg.Credentials.Retrieve(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	payloadHash := sha256.Sum256(requestBody)
+	signer := v4.NewSigner()
+	if err := signer.SignHTTP(ctx, credentials, req, hex.EncodeToString(payloadHash[:]), stsServiceName, region, signingTime); err != nil {
+		return "", err
+	}
+
+	headersJson, err := json.Marshal(req.Header)
 	if err != nil {
 		return "", err
 	}
 
 	awsData := make(map[string]string)
-	awsData["sts_request_method"] = req.HTTPRequest.Method
-	awsData["sts_request_url"] = base64.StdEncoding.EncodeToString([]byte(req.HTTPRequest.URL.String()))
+	awsData["sts_request_method"] = req.Method
+	awsData["sts_request_url"] = base64.StdEncoding.EncodeToString([]byte(req.URL.String()))
 	awsData["sts_request_body"] = base64.StdEncoding.EncodeToString(requestBody)
 	awsData["sts_request_headers"] = base64.StdEncoding.EncodeToString(headersJson)
 	awsDataDump, err := json.Marshal(awsData)
@@ -50,4 +81,27 @@ func GetCloudId() (string, error) {
 
 	cloudId := base64.StdEncoding.EncodeToString(awsDataDump)
 	return cloudId, nil
+}
+
+func resolveSTSEndpoint(ctx context.Context, cfg awssdk.Config, region string) (url.URL, error) {
+	useFIPS := false
+	useDualStack := false
+	useGlobalEndpoint := false
+
+	endpoint, err := sts.NewDefaultEndpointResolverV2().ResolveEndpoint(ctx, sts.EndpointParameters{
+		Region:            &region,
+		UseFIPS:           &useFIPS,
+		UseDualStack:      &useDualStack,
+		Endpoint:          cfg.BaseEndpoint,
+		UseGlobalEndpoint: &useGlobalEndpoint,
+	})
+	if err != nil {
+		return url.URL{}, err
+	}
+
+	endpointURL := endpoint.URI
+	if endpointURL.Path == "" {
+		endpointURL.Path = "/"
+	}
+	return endpointURL, nil
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,14 @@
 module github.com/akeylesslabs/akeyless-go-cloud-id
 
-go 1.26.2
+go 1.26
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.17.0
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.8.0
-	github.com/aws/aws-sdk-go v1.55.5
+	github.com/aws/aws-sdk-go-v2 v1.41.7
+	github.com/aws/aws-sdk-go-v2/config v1.32.17
+	github.com/aws/aws-sdk-go-v2/credentials v1.19.16
+	github.com/aws/aws-sdk-go-v2/service/sts v1.42.1
 	google.golang.org/api v0.216.0
 )
 
@@ -15,6 +18,16 @@ require (
 	cloud.google.com/go/compute/metadata v0.6.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.10.0 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v1.2.2 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.4.24 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.23 // indirect
+	github.com/aws/aws-sdk-go-v2/service/signin v1.0.11 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.30.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.35.21 // indirect
+	github.com/aws/smithy-go v1.25.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -23,7 +36,6 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
